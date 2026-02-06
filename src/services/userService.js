35533be92dda9c5f3d16c71ff115d233d6b25df0
JoaoLavras <joaoantoniogoncalves14@gmail.com>
2026-02-06 07:55:12 -0300

    Envianod alterações e corrigindo erros

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,12 +1,30 @@
 import { input } from '@inquirer/prompts';
 import chalk from 'chalk';
+import { armazenarUser, listarUsersRepository } from '../repositories/userRepository.js';
 
-function validarCadastro(cpf){
+function validarCadastro(cpf, nome, idade){
     console.clear();
-    console.log(`O CPF: ${cpf} chegou para validação em userService`); //tirar depois
+    console.log(`O CPF: ${cpf} chegou para validação em userService`);
+    //tirar depois
     
+    //validar tamanho
+    if(cpf.length !== 11){
+        console.log(cpf.length);
+        console.log("Cpf Invalido - Min 11 caracteres!");
+        return;
+    }
 
+    //validar existencia no sistema
+
+    //armazenar
+    armazenarUser(cpf, nome, idade);
+
+    
 }
 
-export default { validarCadastro }
+function listarUsersService(){
+    return console.log(listarUsersRepository());
+}
+
+export { validarCadastro, listarUsersService }
 
